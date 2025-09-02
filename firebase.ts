a8450e6511e40691732a3a65de603f8e2ec0b2d1
ilix-studio <ilishjyoti17@gmail.com>
2025-09-03 01:42:47 +0530

    env updates/2

--- firebase.ts
+++ firebase.ts
@@ -2,8 +2,9 @@ import admin from "firebase-admin";
 
 admin.initializeApp({
   credential: admin.credential.cert({
-    projectId: "course90-b95d3",
+    projectId: "tsangpool-honda-otp",
     privateKey: process.env.FIREBASE_PRIVATE_KEY?.replace(/\\n/g, "\n"),
-    clientEmail: process.env.FIREBASE_CLIENT_EMAIL,
+    clientEmail:
+      "firebase-adminsdk-fbsvc@tsangpool-honda-otp.iam.gserviceaccount.com",
   }),
 });
