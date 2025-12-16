9437291ec3c38eeac5f25c1f4feb667e96b91db4
ilix-studio <ilishjyoti17@gmail.com>
2025-12-17 02:35:40 +0530

    CORS fix

--- src/config/allowOrigins.ts
+++ src/config/allowOrigins.ts
@@ -4,16 +4,14 @@ const allowOrigins = [
   "http://localhost:5173/admin/addbikes",
   "https://tsangpoolhonda.com/",
   "https://tsangpoolhonda.com",
+  "https://www.tsangpoolhonda.com",
   "https://tsangpoolhonda.com/view-all",
   "https://tsangpoolhonda.com/bikes/search",
   "https://tsangpoolhonda.com/admin/dashboard",
   "https://tsangpoolhonda.com/admin/superlogin",
   "https://tsangpoolhonda.com/admin/managerlogin",
-  "https://honda-site-golaghat-v2.vercel.app",
-  "https://honda-site-golaghat-v2.vercel.app/admin/dashboard",
-  "https://honda-site-golaghat-v2.vercel.app/admin/superlogin",
-  "https://honda-site-golaghat-v2.vercel.app/admin/managerlogin",
   "http://localhost:5173/customer-sign-up",
+  "https://honda-golaghat-dealer-be-874257626954.europe-west1.run.app/",
   "https://identitytoolkit.googleapis.com/v1/accounts:sendVerificationCode?key=AIzaSyCOLCfkbNXvivcQVujbHOx51697D84BE1g",
 ];
 
